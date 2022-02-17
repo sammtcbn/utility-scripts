ab3b010f96506f6f552cd87717f1002abefd92f0
sammtcbn <sammtcbn@gmail.com>
2022-02-17 16:13:25 +0800

    fix wording

--- install.sh
+++ install.sh
@@ -122,20 +122,10 @@ fi
 
 if [ "${ip}" == "localhost" ]; then
     echo install to ${destpath} ...
-    install_to_local containerd-script
-    install_to_local docker-script
-    install_to_local helm-script
-    install_to_local kubectl-script
-    install_to_local microk8s-script
-    install_to_local minikube-script
+    install_to_local linux
 else
     echo install to ${id}@${ip}:${destpath} ...
-    install_to_remote containerd-script
-    install_to_remote docker-script
-    install_to_remote helm-script
-    install_to_remote kubectl-script
-    install_to_remote microk8s-script
-    install_to_remote minikube-script
+    install_to_remote linux
 fi
 
 echo "done"
